3c52a154eb561c1780622c4682c9de469221e50d
Benjamin Chang <42145150+BenjaminCCG@users.noreply.github.com>
2023-08-18 21:20:53 +0800

    feat(组件): 接口时长

--- src/network/axios/axios-setup.ts
+++ src/network/axios/axios-setup.ts
@@ -3,5 +3,5 @@ import { AxiosRequestConfig } from 'axios';
 /** 定义axios基础配置 */
 export const axiosBaseOptions: AxiosRequestConfig = {
   baseURL: import.meta.env.VITE_API_BASE_URL as string, // 如果本地配置了跨域 那么.env.development VITE_API_BASE_URL请修改为你的替换字符串 例如'/api'
-  timeout: 20000
+  timeout: 60000
 };
